08a8736cd5afbb29dc899b7ae57091a4d41df251
Gabriel <gabriellopesbusiness@gmail.com>
2022-04-28 00:09:38 -0300

    change variable name

--- sort/insertion-sort/insertionSort.js
+++ sort/insertion-sort/insertionSort.js
@@ -1,13 +1,13 @@
 function insertionSort(arr) {
     let array = [...arr];
-    for (let j = 1; j < array.length; j++) {
-        let key = array[j];
-        i = j - 1;
-        while (i >= 0 && array[i] > key) {
-            array[i + 1] = array[i];
-            i = i - 1;
+    for (let currentItem = 1; currentItem < array.length; currentItem++) {
+        let key = array[currentItem];
+        currentIndex = currentItem - 1;
+        while (currentIndex >= 0 && array[currentIndex] > key) {
+            array[currentIndex + 1] = array[currentIndex];
+            currentIndex = currentIndex - 1;
         }
-        array[i + 1] = key;
+        array[currentIndex + 1] = key;
     }
     return array;
 }
